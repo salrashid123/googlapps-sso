651584a441ff929c09773ef59094b77ed8cc2b27
salrashid123 <salrashid123@gmail.com>
2015-08-12 09:33:19 -0700

    Update apps.py
    update instructions for mapping volumes

--- apps.py
+++ apps.py
@@ -40,7 +40,7 @@ To use:
           Logout: https://sso.yourdomain.com:28080/logout
           Change Password: https://sso.yourdomain.com:28080/passwd
       --> upload the public cert (ssl.crt)
-  3. If you are running the docker container locally,
+  3. If you are running the docker container on your laptop, you need to speicfy the host where your IdP is running,
       --> On your laptop edit
           /etc/hosts
           127.0.0.1 localhost sso.yourdomain.com
@@ -48,14 +48,15 @@ To use:
   5. make a folder called sso and copy all the files from the github repo into it.
   6. Create the docker container
           docker build -t sso .
-  7. Run the container
-          docker run -t -p 28080:28080 sso --debug  --use_ssl --cert_file=ssl.crt --key_file=ssl.key --key_blank_pwd
-  8. At this point, the IDP is running locally on port sso.yourdomain.com:28080
-  9. If you attempt a new login to https://mail.google.com/a/yourdomain.com, you will get redirected to a login screen on your IDP
-  10. The IDP will authenticate **ANY** user in your apps domain so if you have a user called user1@yourdomain.com, enter in 'user1', any password
+  7. Copy the certificates to /tmp/certs (for example) so that the local certs are visible to the container.        
+  8. Run the container
+          docker run -t -p 28080:28080 -v /tmp/certs/:/certs/:ro salrashid123/appssso --debug --use_ssl --cert_file=/certs/ssl.crt --key_file=/certs/ssl.key --key_blank_pwd
+  9. At this point, the IDP is running locally on port sso.yourdomain.com:28080
+  10. If you attempt a new login to https://mail.google.com/a/yourdomain.com, you will get redirected to a login screen on your IDP
+  11. The IDP will authenticate **ANY** user in your apps domain so if you have a user called user1@yourdomain.com, enter in 'user1', any password
       and yourdomain.com in the IDP login screen
-  11. If successful, you will get redirected to the SAML POST binding screen so  you can see the actual XML signed POST text.
-  12. Click contineu and if the sigatures and validUntil= parameters are ok, you will be logged in as user1
+  12. If successful, you will get redirected to the SAML POST binding screen so  you can see the actual XML signed POST text.
+  13. Click continue and if the sigatures and validUntil= parameters are ok, you will be logged in as user1
 
 
 If you want to generate your own keypairs:
